3344a839820cdb15688eeec3eed6e8ccab47f854
meks2016 <meks2016app@gmail.com>
2017-03-31 22:05:09 +0200

    ermin change 2

--- app/src/main/java/com/apps/meks/arduinoapp/MainActivity.java
+++ app/src/main/java/com/apps/meks/arduinoapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("test", "test");
+        Log.e("test2", "test2");
     }
 }
